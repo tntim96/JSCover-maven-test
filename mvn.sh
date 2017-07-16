64102714302feb13577dfedcf300cd8960fdc481
tntim96 <tntim96@gmail.com>
2017-07-16 20:28:56 +1000

    Release 2.0.0

--- mvn.sh
+++ mvn.sh
@@ -1,2 +1,3 @@
-mvn dependency:copy-dependencies -DoutputDirectory=. -X
-java -cp "JSCover-1.1.0.jar:rhino-1.7.7.1.jar" jscover.Main -V
+rm *.jar
+mvn dependency:copy-dependencies -DoutputDirectory=.
+java -cp "./*" jscover.Main -V
