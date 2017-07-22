dd2427f8e84e56b92a1be803afeb5a4601536c34
tntim96 <tntim96@gmail.com>
2017-07-23 08:16:21 +1000

    Release 2.0.1

--- mvn.sh
+++ mvn.sh
@@ -1,3 +1,11 @@
 rm *.jar
 mvn dependency:copy-dependencies -DoutputDirectory=.
+
 java -cp "./*" jscover.Main -V
+rc=$?; if [[ $rc != 0 ]]; then exit $rc; fi
+
+java -cp "./*" jscover.Main -io test.js
+rc=$?; if [[ $rc != 0 ]]; then exit $rc; fi
+
+java -cp "./*" jscover.report.Main --merge report1 report2 report-merged
+rc=$?; if [[ $rc != 0 ]]; then exit $rc; fi
